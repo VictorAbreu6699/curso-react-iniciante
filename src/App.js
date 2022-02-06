a7b7f9cb3553fd2db76de44dcf7d6742488d8bf2
Victor <azevedovictor6699@gmail.com>
2022-02-06 12:09:06 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TaskList from './components/TaskList/TaskList';
+
+import NavBar from './components/Navbar/Navbar';
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+
+  return idAcc;
+}
 
 function App() {
+
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    }
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask]
+    })
+  }
+
+  const updateTask = (id, title, state) => {    
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if(task.id == id)
+          return {...task, title, state};
+        else
+          return task;
+      })
+    })
+  }
+
+  const deleteTask = (id) => {
+    setTasks((existingTasks) => {
+      return existingTasks.filter((task) => task.id !== id)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />    
+      <div className='container'>
+        <TaskList title="Pendente" taskState={"Pendente"} onAddTask = {addTask} tasks={tasks.filter((t) => t.state === "Pendente")} onTaskUpdate={updateTask} onDeleteTask={deleteTask} />        
+        <TaskList title="Fazendo" taskState={"Fazendo"} onAddTask = {addTask} tasks={tasks.filter((t) => t.state === "Fazendo")} onTaskUpdate={updateTask} onDeleteTask={deleteTask} />
+        <TaskList title="Completa" taskState={"Completa"} onAddTask = {addTask} tasks={tasks.filter((t) => t.state === "Completa")} onTaskUpdate={updateTask} onDeleteTask={deleteTask} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+function NavBar() {
+    return(
+        <nav className='navbar'>
+            <span>Lista de tarefas com React</span>            
+        </nav>
+    )
+}
+
+export default NavBar;
